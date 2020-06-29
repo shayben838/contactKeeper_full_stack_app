8338c73fc8acd5dae60fdb4524fc82638b0e5d41
shay <shayben838@gmail.com>
2020-06-29 12:15:15 +0300

    remove init route

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/contacts", contactsRouter);
