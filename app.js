391a808b9e2ba4a7afd2656a92e82b1447908252
shay <shayben838@gmail.com>
2020-06-29 12:23:32 +0300

    remove init route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
-const path = require("path");
 
 const connectDB = require("./config/db");
 // NPM RUN COMMAND
